e0605992a9e184df60e9f59df72ad0b270cd49a1
John Wick <johnwick2614@gmail.com>
2025-07-06 19:14:06 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className='card-container'>
+        <div className='card'>
+          <h1>Title: Card1</h1>
+          <p>Content: Content 1</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
